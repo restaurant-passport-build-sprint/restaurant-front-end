10ece4962dbd78ce57c335f35e0f83d924ccc4b9
Robert <rc3331@gmail.com>
2019-09-23 16:46:37 -0500

    styled restaurant cards

--- restaurant-passport/src/Components/RestaurantCard.js
+++ restaurant-passport/src/Components/RestaurantCard.js
@@ -1,12 +1,58 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const Restaurant = styled.div`
+  width: 30%;
+  padding-top: 25%;
+  background-color: #FFECBE;
+  position:relative;
+  border-radius: 5px;
+  border: 4px solid #39491C;
+  margin: 1%;
+
+  .card-text{
+    box-sizing:border-box;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    color: #39491C;
+
+    h2{
+        font-size:2.2rem;
+        text-decoration:underline;
+    }
+
+    p{
+        text-align:left;
+        padding-left: 3%;
+        font-size: 1.1rem;
+    }
+
+    .fas{
+        position:absolute;
+        font-size: 6rem;
+        opacity: 0.2;
+        right:5%;
+        bottom:8%;
+        color: red;
+    }
+}
+`;
 
 export default function RestaurantCard(props) {
-    return (
-        <div id={props.id}>
-            <h2>Name: {props.name}</h2>
-            <p>Address: {props.address}, {props.city}, {props.state}, {props.zip}</p>
-            <p>Rating: {props.rating}</p>
-            <p>Type: {props.type}</p>
-        </div>
-    )
+  return (
+    <Restaurant id={props.id}>
+      <div className="card-text">
+        <i class="fas fa-pepper-hot"></i>
+        <h2>{props.name}</h2>
+        <p>
+          Address: {props.address}, {props.city}, {props.state}, {props.zip}
+        </p>
+        <p>Rating: {props.rating}</p>
+        <p>Type: {props.type}</p>
+      </div>
+    </Restaurant>
+  );
 }
--- restaurant-passport/src/Components/RestaurantList.js
+++ restaurant-passport/src/Components/RestaurantList.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import { data } from "../restaurant.js"
-import RestaurantCard from "./RestaurantCard"
+import React from "react";
+import { data } from "../restaurant.js";
+import RestaurantCard from "./RestaurantCard";
+import styled from "styled-components";
+
+const CardList = styled.div`
+  width: 50%;
+  max-width: 1080px;
+  min-width: 400px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  background-color: #F5F5F5;
+`;
 
 export default function RestaurantList() {
-    return (
-        <div>
-            {data.map(item => 
-            <RestaurantCard id={item.id} name={item.name} address={item.address} city={item.city} state={item.state} zip={item.zip} rating={item.rating} type={item.type} />
-            )}
-        </div>
-    )
+  return (
+    <div>
+      <h1>Visited Restaurants</h1>
+      <CardList>
+        {data.map(item => (
+          <RestaurantCard
+            id={item.id}
+            name={item.name}
+            address={item.address}
+            city={item.city}
+            state={item.state}
+            zip={item.zip}
+            rating={item.rating}
+            type={item.type}
+          />
+        ))}
+      </CardList>
+    </div>
+  );
 }
