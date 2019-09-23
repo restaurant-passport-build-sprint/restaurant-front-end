79eed56e084b14fd74369c0968365d75cc22d22b
Robert <rc3331@gmail.com>
2019-09-23 12:02:23 -0500

    yarn install to fix error

--- restaurant-passport/src/App.js
+++ restaurant-passport/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import FormikRegisterForm from './Components/signup';
+import Header from "./Components/Header"
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <FormikRegisterForm/>
     </div>
   );
