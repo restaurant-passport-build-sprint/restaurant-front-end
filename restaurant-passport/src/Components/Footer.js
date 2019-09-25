986e27ad24e041582e63e067b3712efcf26c45e7
Robert <rc3331@gmail.com>
2019-09-25 10:39:00 -0500

    changes to footer

--- restaurant-passport/src/Components/Footer.js
+++ restaurant-passport/src/Components/Footer.js
@@ -4,31 +4,35 @@ import styled from "styled-components";
 const FooterContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items:center;
+  align-items: center;
   padding: 0 1vw;
-  background-color: #F5F5F5;
+  background-color: #f5f5f5;
   height: 7vh;
-  position:absolute;
-  bottom:0;
-  width:100%;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
 
-  .fas{
-      font-size: 2.2rem;
-      color: black;
-      opacity: 0.5;
-      margin-left: 10px;
+  .fas {
+    font-size: 2.2rem;
+    color: black;
+    opacity: 0.5;
+    margin-left: 10px;
   }
-  .fas:hover{
-      transform: scale(1.3);
-      transition: 0.4s;
+  .fas:hover {
+    transform: scale(1.3);
+    transition: 0.4s;
   }
 `;
 export default function Footer() {
   return (
     <FooterContainer>
       <div>
-        
-        <p>Need Help? 24/7 Live Chat Available <a href="#"><i className="fas fa-comment"></i></a></p>
+        <p>
+          Need Help? 24/7 Live Chat Available{" "}
+          <a href="#">
+            <i className="fas fa-comment"></i>
+          </a>
+        </p>
       </div>
       <p>&copy;2019 FoodiePass</p>
     </FooterContainer>
